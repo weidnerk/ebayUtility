91f7b22a681cf31278de4aca0d84bed8c74b7fc9
Kevin Weidner <weidnerk@gmail.com>
2020-03-01 13:13:18 -0500

    Add CanList().

--- eBayUtility/FetchSeller.cs
+++ eBayUtility/FetchSeller.cs
@@ -740,6 +740,21 @@ namespace eBayUtility
             }
             return ret;
         }
-
+        /// <summary>
+        /// Server side supplier item validation - is this where function belongs?
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="allowedDeliveryDays"></param>
+        public static void CanList(SupplierItem item, int allowedDeliveryDays)
+        {
+            if (item.Arrives.HasValue)
+            {
+                int days = dsutil.DSUtil.GetBusinessDays(DateTime.Now, item.Arrives.Value);
+                if (days > allowedDeliveryDays)
+                {
+                    item.CanList = 1;
+                }
+            }
+        }
     }
 }
