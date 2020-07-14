d35b20b0ae8ae4289c19e31e2afbd4caa01c3848
Kevin Weidner <weidnerk@gmail.com>
2020-07-14 15:30:52 -0400

    Add test code to Analysis.

--- eBayUtility/StoreCheck.cs
+++ eBayUtility/StoreCheck.cs
@@ -79,6 +79,10 @@ namespace Utility
                 // scan each item in store - is it in db?
                 foreach (ItemType oItem in storeItems)
                 {
+                    //if (oItem.ItemID == "224079107216")
+                    //{
+                    //    int stop = 99;
+                    //}
                     var listing = Utility.StoreCheck.LookupItemid(settings, oItem.ItemID);
                     if (listing == null)
                     {
